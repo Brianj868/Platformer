469a2951238bf5e055ff2edac3769a076ca7501a
Brian Johnston <brianj868@yahoo.com>
2023-01-09 17:40:16 -0800

    Added Breakable script

---
+++ Assets/Scripts/Breakable.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Breakable : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.GetComponent<Player>() == null)
+            return;
+
+        if (collision.contacts[0].normal.y > 0)
+            TakeHit();
+    }
+
+    private void TakeHit()
+    {
+        gameObject.SetActive(false);
+    }
+}
