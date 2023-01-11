53706f81f6f1d4411393fbcf0c16a925d129ace0
Brian Johnston <brianj868@yahoo.com>
2023-01-10 17:19:24 -0800

    Added door that opens with debug menu

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] Sprite _openMid;
+    [SerializeField] Sprite _openTop;
+
+    [SerializeField] SpriteRenderer _rendererMid;
+    [SerializeField] SpriteRenderer _rendererTop;
+
+    [ContextMenu("Open Door")]
+    void open()
+    {
+        _rendererMid.sprite = _openMid;
+        _rendererTop.sprite = _openTop;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
