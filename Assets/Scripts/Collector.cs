47cfb4ae57d810b439c90210de4645fb829cce98
Brian Johnston <brianj868@yahoo.com>
2023-01-17 19:10:01 -0800

    Added some UI elements to collector

--- Assets/Scripts/Collector.cs
+++ Assets/Scripts/Collector.cs
@@ -1,24 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Collector : MonoBehaviour
 {
     [SerializeField] Collectible[] _collectibles;
+
+    TMP_Text _remainingText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _remainingText = GetComponentInChildren<TMP_Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        int countRemaining = 0;
         foreach(var collectible in _collectibles)
         {
             if (collectible.isActiveAndEnabled)
-                return;
+                countRemaining++;
         }
+
+        _remainingText?.SetText(countRemaining.ToString());
+
+        if (countRemaining > 0)
+            return;
 
         Debug.Log("Got All Gems");
     }
