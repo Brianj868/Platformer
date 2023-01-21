22cdda283382b9d67f2256a275fb35669c99cbe4
Brian Johnston <brianj868@yahoo.com>
2023-01-20 23:45:47 -0800

    Used Inheritance to make the boxes work

--- Assets/Scripts/HittableFromBelow.cs
+++ Assets/Scripts/HittableFromBelow.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 public class HittableFromBelow : MonoBehaviour
 {
     [SerializeField] protected Sprite _usedSprite;
+    Animator _animator;
 
     protected virtual bool CanUse => true;
+
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -18,11 +24,18 @@ public class HittableFromBelow : MonoBehaviour
 
         if (collision.contacts[0].normal.y > 0)
         {
+            PlayAnimation();
             Use();
 
             if (CanUse == false)
                 GetComponent<SpriteRenderer>().sprite = _usedSprite;
         }
+    }
+
+    private void PlayAnimation()
+    {
+        if (_animator != null)
+            _animator.SetTrigger("Use");
     }
 
     protected virtual void Use()
