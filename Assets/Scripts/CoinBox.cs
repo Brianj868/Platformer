b63efc422a8480768d2dfaaf6c4e1bd95220af85
Brian Johnston <brianj868@yahoo.com>
2023-01-20 17:30:14 -0800

    Added a HittableFromBelow base class for coinbox and item box

--- Assets/Scripts/CoinBox.cs
+++ Assets/Scripts/CoinBox.cs
@@ -2,32 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoinBox : MonoBehaviour
+public class CoinBox : HittableFromBelow
 {
     [SerializeField] int _totalCoins = 3;
-    [SerializeField] Sprite _usedSprite;
 
     int _remainingCoins;
+
+    protected override bool CanUse => _remainingCoins > 0;
 
     private void Start()
     {
         _remainingCoins = _totalCoins;
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
+    protected override void Use()
     {
-        var player = collision.collider.GetComponent<Player>();
-        if (player == null)
-            return;
+        base.Use();
 
-        if (collision.contacts[0].normal.y > 0 && _remainingCoins > 0)
-        {
-            Coin.CoinsCollected++;
-            _remainingCoins--;
-            if (_remainingCoins <= 0)
-            {
-                GetComponent<SpriteRenderer>().sprite = _usedSprite;
-            }
-        }
+        Coin.CoinsCollected++;
+        _remainingCoins--;
     }
 }
