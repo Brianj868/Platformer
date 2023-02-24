8873fe2dd145f427ff067340be00e6999d533a32
Brian Johnston <brianj868@yahoo.com>
2023-02-23 22:14:41 -0800

    Added music using singleton pattern

---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public static Music Instance { get; private set; }
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
