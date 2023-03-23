f59c944a3afb47efa89e701eb48376bda9c53893
Brian Johnston <brianj868@yahoo.com>
2023-03-22 17:06:49 -0700

    Launching fireballs from input

--- Assets/Scripts/FireballLauncher.cs
+++ Assets/Scripts/FireballLauncher.cs
@@ -6,15 +6,23 @@ public class FireballLauncher : MonoBehaviour
 {
     [SerializeField] Fireball _fireballPrefab;
 
+    Player _player;
+    string _fireButton;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        Instantiate(_fireballPrefab, transform.position, Quaternion.identity);
+        _player = GetComponent<Player>();
+        _fireButton = $"P{_player.PlayerNumber}Fire";
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown(_fireButton))
+        {
+            Instantiate(_fireballPrefab, transform.position, Quaternion.identity);
+        }
         
     }
 }
